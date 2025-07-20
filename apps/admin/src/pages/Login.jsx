e5692715db29e189fea4e51a0f2544b868ed5f50
Chipyoka <mwapechipyoka18@gmail.com>
2025-07-20 23:40:34 +0200

    Auth logic done/logina page responsive

--- apps/admin/src/pages/Login.jsx
+++ apps/admin/src/pages/Login.jsx
@@ -1,6 +1,9 @@
 import { useNavigate } from 'react-router-dom';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import useAuthStore from '../store/authStore';
+
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../../../shared/firebase';
 
 
 import { ArrowRight } from 'lucide-react';
@@ -8,59 +11,117 @@ import { UserCircle } from 'lucide-react'
 import { Lock } from 'lucide-react';
 
 
-import Logo from '../assets/eps-white.png'; // Adjust the path as necessary
-
+import Logo from '../assets/eps-white.png'; 
 const Login = () => {
   const login = useAuthStore((state) => state.login);
   const navigate = useNavigate();
 
+const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   const [loading, setLoading] = useState(false);
-  const [showPassword, setShowPassword] = useState(true);
+  const [showPassword, setShowPassword] = useState(false);
+  const [message, setMessage] = useState({});
 
 
 //   handle show password
   const handleShowPassword = () =>{
     setShowPassword(!showPassword);
     console.log("Show Password: ", showPassword);
+
+    if(showPassword === false){
+
+        const msg = {
+            type : "warning",
+            text : "Your password is now visible"
+        }
+
+        setMessage(msg);
+    }else{
+        setMessage({});
+    }
+
   }
 
 //  handle login
-  const handleLogin = () => {
+   const handleLogin = async (e) => {
+    e.preventDefault();
+    setMessage({});
+    //validate inputs
+    if(!email || !password){
+         setMessage({
+            type: 'error',
+            text: 'All fields are reguired'
+        })
 
-    // Set loading state
+        return
+    }
+
+    // validate password
+    if(password.length < 8){
+        setMessage({
+            type: 'error',
+            text: 'Password must be longer than 8 characters'
+        })
+        return
+    }
+
+    //after validation, setloading
     setLoading(true);
-    setTimeout(() => {
+
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      login(userCredential.user);
+      navigate('/'); // or /dashboard
+    } catch (err) {
+      console.error(err.message);
+      setMessage({
+        type: 'error',
+        text: 'Invalid credentials. Please try again.'
+      });
+    } finally {
       setLoading(false);
-
-      // Simulate a login request
-        const dummyUser = { id: 1, name: 'Admin' }; // Replace with real logic
-        login(dummyUser);
-        navigate('/');
-    }, 2000); // Simulate a delay for the login process
-
-    
+    }
   };
+
 
   return (
     <div className="text-gray-500 flex flex-col items-center justify-center w-full max-w-full h-screen bg-[url('../assets/classroom-bg-min.png')] bg-no-repeat bg-cover bg-center">
       <div className="absolute inset-0 bg-black/40  z-0"></div>
 
-      <div className="bg-white shadow-lg z-10 w-md">
+      <div className="bg-white shadow-lg z-10 md:w-md w-sm">
         <div className="bg-primary flex items-center justify-center p-4 mb-4">
             <img src={Logo} alt="Exam proctoring system" className="h-[33px]" />
         </div>
+       {message && <div className="w-full text-center text-xs font-semibold">
+            <p className={
+                message.type === 'warning' ? 'text-yellow-500' :
+                message.type === 'info'    ? 'text-blue-500'   :
+                message.type === 'error'   ? 'text-red-500'    :
+                'text-gray-700'
+                }>
+                {message.text}
+            </p>
+        </div>}
 
         <div className="p-8 flex flex-col items-center justify-center">
 
-            <h2 className="text-gray-700 text-2xl font-semibold">Admin Login</h2>
-            <p className="text-sm text-gray-500 mb-6">To continue, provide you login credentials below.</p>
+            <h2 className="text-gray-700 text-2xl font-semibold ">Admin Login</h2>
+            <p className="text-sm text-gray-500 mb-6 text-center">To continue, provide you login credentials below.</p>
 
-            <form action="" className="w-full">
+            <form onSubmit={handleLogin} className="w-full">
                 <div className="input-group relative flex items-center border border-gray-300 px-3 py-2 transition-colors duration-200 focus-within:border-blue-500 focus-within:ring-2 focus-within:ring-blue-100">
                     <div className="bg-gray-100 p-2">
                         <UserCircle className="w-6 h-6 text-gray-500" />
                     </div>
-                    <input type="text" placeholder="Username" name="username" className="text-gray-700 ml-2 w-full border-none outline-none bg-transparent focus:ring-0 focus:outline-none placeholder:text-sm"/>
+                    <input 
+                    type="email" 
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    
+                    placeholder="Email" 
+                    name="email" 
+                    className="text-gray-700 ml-2 w-full border-none outline-none bg-transparent focus:ring-0 focus:outline-none placeholder:text-sm"/>
                 </div>
 
                 {/* Password input */}
@@ -68,21 +129,28 @@ const Login = () => {
                     <div className="bg-gray-100 p-2">
                         <Lock className="w-6 h-6 text-gray-500" />
                     </div>
-                    <input type={showPassword ? 'text' : 'password'} placeholder="Password" name="password" className="text-gray-700 ml-2 w-full border-none outline-none bg-transparent focus:ring-0 focus:outline-none placeholder:text-sm"/>
+                    <input 
+                    type={showPassword ? 'text' : 'password'} 
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    
+                    min="8" 
+                    placeholder="Password" 
+                    name="password" 
+                    className="text-gray-700 ml-2 w-full border-none outline-none bg-transparent focus:ring-0 focus:outline-none placeholder:text-sm" title="Required with minmum of 8 characters"/>
                 </div>
                 <p  
                     onClick={handleShowPassword} 
                     className="hyperlink text-xs mt-2 w-fit no-select"
                     >
                         {showPassword ? 'Hide' : 'Show'} password
-                    </p>
+                </p>
 
 
                 {/* Login button */}
                 <button 
                     type="submit"
                     className="w-full mt-6 flex items-center justify-center gap-x-4 btn-primary transition-colors duration-300"
-                    onClick={handleLogin}
                     disabled={loading} // optional: disables button while loading
                 >
                     {/* Show loader only if loading is true */}
